f78482173ce3e4484c00035f38b60538ea46c4e2
Jo-fai Chow <jofai.chow@gmail.com>
2019-07-09 15:17:43 +0200

    Added mlbench

--- setup.R
+++ setup.R
@@ -1,6 +1,7 @@
 # Install R packages for this tutorial
 
-pkgs <- c("h2o", "DALEX", "breakDown", "pdp", "knitr", "rmdformats", "DT", "xgboost")
+pkgs <- c("h2o", "DALEX", "breakDown", "pdp",
+          "knitr", "rmdformats", "DT", "xgboost", "mlbench")
 for (pkg in pkgs) {
   if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
 }
