9d0e4980d3d9ad703ada8c0cb8456bb25c3c3c2e
Jo-fai Chow <jofai.chow@gmail.com>
2019-07-09 15:14:39 +0200

    Added xgboost

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 # Install R packages for this tutorial
 
-pkgs <- c("h2o", "DALEX", "breakDown", "pdp", "knitr", "rmdformats", "DT")
+pkgs <- c("h2o", "DALEX", "breakDown", "pdp", "knitr", "rmdformats", "DT", "xgboost")
 for (pkg in pkgs) {
   if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
 }
